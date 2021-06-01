4880719bcb31db97e085f32ded3c9e5a1b33ba74
Jacqueline Zavala <a01750480@itesm.mx>
2021-05-31 21:24:30 -0500

    Comentarios
    Se agregaron los comentarios.

--- Resaltador Paralelo/resaltadorParalelo.py
+++ Resaltador Paralelo/resaltadorParalelo.py
@@ -1,29 +1,64 @@
+# Resaltador de Texto: Implementación paralela
+# Edna Jacqueline Zavala Ortega A01750480
+# Erick Alberto Bustos Cruz A01378966
+
 import resaltadorTexto as rt
 import os
 import functools
 from concurrent.futures import ProcessPoolExecutor
 import time
 
-def main(directory,sExpressions,cores, extension):
+def main(directory,sExpressions,cores, fileExtension):
+    """
+    Función que recibe un string "directorio" con el path de un directorio determinado,
+    un string "sExpressions" con el nombre de un archivo que contiene expresiones S que
+    representan las categorías léxicas de un lenguaje de programación, un entero "cores"
+    que indica el número de núcleos que posee la computadora del usuario y un string
+    "fileExtension" que indica qué tipo de archivos se desean resaltar.
+    La función genera un directorio de nombre "Resultados"(o con variantes de ese nombre),
+    que contiene archivos html, donde se realiza un resaltado de texto para cada código
+    con extensión "fileExtension" existente dentro de "directory".
+    La implementación de este resaltador hace uso de procesos paralelos que hacen uso
+    de todos los "cores" disponibles en el procesador para maximizar la eficiencia.
+    """
+    # Nombre tentativo del directorio donde se almacenarán los códigos resaltados 
     directoryName = "Resultados"
-    directoryName = rt.defineDirectory(directoryName)
-   
+    # Permite definir el nombre del directorio que se creará para almacenar los scripts resaltados.
+    directoryName = rt.defineDirectory(directoryName) 
+    # Lectura de las expresiones-s que permiten identificar las categorías léxicas del lenguaje.
     data = rt.expressionsFile(sExpressions)
+    # Permite almacenar los nombres que se les asignarán a los archivos html resaltados.
     htmlNames = []
-    files = rt.findFiles(directory,[], extension)
-    size = len(files)//cores
-    if size < 1:
+    # Se localizan todos los archivos .py que se permitan.
+    files = rt.findFiles(directory,[], fileExtension) 
+    # Define de manera dinámica el tamaño del "chunk" que tomará cada proceso.
+    size = len(files)//cores 
+    
+    # Evita que el tamaño del "chunk" sea inválido.
+    if size < 1: 
         size = 1
+    
+    # Crea y se cambia al directorio en el que se almacenarán los scripts resaltados.
     os.mkdir(directoryName)
     os.chdir(directoryName)
+    
+    # Agrega a la lista htmlNames los nombres de los archivos html que se crearán como resultado del resaltado.
     for name in files:
         htmlNames.append(rt.nameHTML(name))
-    with ProcessPoolExecutor(max_workers=6) as executor:        
+    
+    # Se crean tantos procesos como cores se haya especificado que tiene el procesador y le envía a cada uno un "chunk"
+    # de tamaño "size" para que lo procese con la función parcial derivada de la función principal del textHighlighter.
+    # Nota: dicho cambio fue necesario dado que dos de los argumentos son estáticos y no iterables. 
+    with ProcessPoolExecutor(max_workers = cores) as executor:        
         newFun = functools.partial(rt.textHighlighter, data[0], data[1])
         executor.map(newFun, files, htmlNames, chunksize = size)
-    
-        
-if __name__ == '__main__': 
+     
+if __name__ == '__main__':
+    """
+    Llama al método main y permite calcular el tiempo que le toma resaltar
+    todos los scripts considerando la extensión y las expresiones proporcionadas.
+    Imprime el tiempo de ejecución.
+    """
     startTime = time.time()
     main(os.getcwd(),'expresionesS.txt', 6, '.py')
     finishTime = time.time() - startTime
--- Resaltador Paralelo/resaltadorTexto.py
+++ Resaltador Paralelo/resaltadorTexto.py
@@ -8,7 +8,7 @@ import os
 def nameHTML(file):
     """
     Función que recibe un string con terminación .py
-    y la cambia por la terminación .html
+    la cual cambia por la terminación .html
     """
     fileName = os.path.split(file) 
     return fileName[1][:-2] + "html"
@@ -16,13 +16,18 @@ def nameHTML(file):
 def findFiles(directory,files, extension):
     """
     Función que recibe un string  "directory" el cual contiene la dirección de un directorio
-    y una lista de strings denominada "files". La función agrega las direcciones de todos los 
-    archivos con extensión ".py" presentes en el directorio dado a la lista "files",
+    y una lista de strings denominada "files". La función agrega las direcciones de todos los
+    archivos con extensión "extension" presentes en el directorio dado a la lista "files",
     incluyendo aquellos en directorios anidados. Al final del proceso, se regresa la lista
     "files".
     """
+    # Instanciarse en el directorio actual
     os.chdir(directory)
-    currentDirectory = os.listdir(directory) #['Codigos', 'Imagenes', 'code.py', 'imagen.jpg']
+    
+    # Listar elementos del dorectorio actual
+    currentDirectory = os.listdir(directory)
+    
+    # Iterar sobre elementos
     for element in currentDirectory:
         if extension in element:
             # Agregar path de archivo .py a files
@@ -41,7 +46,6 @@ def defineDirectory(directoryName):
     "directoryName" concatenado con -1. En caso contrario, regresa únicamente "directoryName"
     """
     if directoryName not in os.listdir(os.getcwd()):  
-        #print(directoryName)
         return directoryName
     else:
         directoryName += "-1"
@@ -167,7 +171,6 @@ def textHighlighter(labels, regexps, codeFile, htmlName):# sExpressions = "../ex
     Como resultado, la función crea un documento "Resultado.html" donde escribe el código de "codeFile"
     coloreando cada elemento perteneciente a una categoría léxica de un color diferente.
     """
-    
 
     # Crear archivo html donde se va a guardar el resultado
     f = open(htmlName, 'w', encoding = 'utf8')
